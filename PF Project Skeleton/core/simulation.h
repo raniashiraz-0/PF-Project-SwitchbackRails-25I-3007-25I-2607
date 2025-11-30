ca61ff3fc2626a61e038b133a316b46b70008cbe
raniashiraz-0 <i253007@isb.nu.edu.pk>
2025-11-30 15:37:24 +0500

    Update simulation.h

--- PF Project Skeleton/core/simulation.h
+++ PF Project Skeleton/core/simulation.h
@@ -4,25 +4,30 @@
 // ============================================================================
 // SIMULATION.H - Simulation tick logic
 // ============================================================================
-
+void SpawnTrains(int g_currenttick);                    	
+int CalculateManhattanDistance(int x1, int y1, int x2, int y2); 
+bool SolvePriority(int trainid, int destinationx, int destinationy); 
 // ----------------------------------------------------------------------------
 // MAIN SIMULATION FUNCTION
 // ----------------------------------------------------------------------------
 // Run one simulation tick.
-void simulateOneTick();
+void simulateOneTick(g_grid,g_trains [],g_traincount,g_switches[],g_switchcount, g_currenttick );
 
 // ----------------------------------------------------------------------------
 // INITIALIZATION
 // ----------------------------------------------------------------------------
 // Initialize the simulation after loading a level.
-void initializeSimulation();
+void initializeSimulation(g_grid,g_trains [],g_traincount,g_switches[],g_switchcount, g_currenttick );
 
 // ----------------------------------------------------------------------------
 // UTILITY
 // ----------------------------------------------------------------------------
 // True if all trains are delivered or crashed.
-bool isSimulationComplete();
-
+bool isSimulationComplete(g_trains [],g_traincount);
+void UpdatingSingnalLight();
+int GetSignalColor(int g_direction);
+int WeatherAffect(g_weather);
+void PauseSimulation();
+void ContinueSimulation();
 #endif
-
 
